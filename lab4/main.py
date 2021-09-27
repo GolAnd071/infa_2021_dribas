3ef0f8041f9b6912ea6e6ef139428dc49059bfbd
DribasRV <dribas.rv@phystech.edu>
2021-09-27 12:02:21 +0300

    Added template for functions for background and bird.

--- lab4/main.py
+++ lab4/main.py
@@ -4,16 +4,38 @@ from pygame.draw import *
 pygame.init()
 
 FPS = 30
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((600, 800))
 
+
+# background
 def bg_bird():
     pass
+
 
 def background():
     pass
 
+
+# bird
+def bird_head():
+    pass
+
+
+def bird_wing():
+    pass
+
+
+def bird_paw():
+    pass
+
+
 def bird():
     pass
+
+
+# fish
+
+
 
 def fish():
     pass
