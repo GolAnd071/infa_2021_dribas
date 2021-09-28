e6dd612ad2e4e9ff2fcae2b86bf6c6fb2204066d
DribasRV <dribas.rv@phystech.edu>
2021-09-28 11:59:50 +0300

    Added horizontal lines in bg.

--- lab4/main.py
+++ lab4/main.py
@@ -3,17 +3,30 @@ from pygame.draw import *
 
 pygame.init()
 
+window_w = 600
+window_h = 800
 FPS = 30
-screen = pygame.display.set_mode((600, 800))
+screen = pygame.display.set_mode((window_w, window_h))
 
 
 # background
-def bg_bird():
+def bg_bird(x, y, angle):
     pass
 
 
-def background():
-    pass
+def bg():
+    h1 = window_h * 0.1
+    h2 = window_h * 0.05
+    h3 = window_h * 0.1
+    h4 = window_h * 0.15
+    h5 = window_h * 0.1
+    h6 = window_h * 0.5
+    rect(screen, (33,33,120), (0, 0, window_w, h1))
+    rect(screen, (141,95,211), (0, h1, window_w, h2))
+    rect(screen, (205,135,222), (0, h2, window_w, h3))
+    rect(screen, (222,135,170), (0, h3, window_w, h4))
+    rect(screen, (255,153,85), (0, h4, window_w, h5))
+    rect(screen, (0,102,128), (0, h5, window_w, h6))
 
 
 # bird
@@ -34,12 +47,26 @@ def bird():
 
 
 # fish
+def fish_eye():
+    pass
 
+
+def fish_head():
+    pass
+
+
+def fish_fin():
+    pass
+
+
+def fish_body():
+    pass
 
 
 def fish():
     pass
 
+bg()
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
@@ -49,5 +76,6 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+
 
 pygame.quit()
